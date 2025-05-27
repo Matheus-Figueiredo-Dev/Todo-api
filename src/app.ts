3cb2f24b7e205fbd8425fe338a103c0b78d4b548
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-27 15:01:02 -0300

    First commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import router from './routes/routes';
 
 class App {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -7,11 +8,15 @@ class App {
   constructor() {
     this.app = express();
     this.middlewares();
+    this.routes();
   }
 
   middlewares() {
     this.app.use(express.json());
     this.app.use(cors());
+  }
+  routes() {
+    this.app.use(router);
   }
 }
 
