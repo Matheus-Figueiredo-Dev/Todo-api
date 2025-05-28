9aedf753880eeed4ffbd8b50a56a669ddf1e5db3
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-28 11:48:55 -0300

    Atualizando e deletando tarefas

--- src/api/controllers/todoController.ts
+++ src/api/controllers/todoController.ts
@@ -14,7 +14,7 @@ class TodoController {
   }
 
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  async createTodos(req: Request, res: Response): Promise<any> {
+  async createTodo(req: Request, res: Response): Promise<any> {
     try {
       const { task } = req.body;
       if (!task) {
@@ -29,6 +29,38 @@ class TodoController {
       await todo.save();
 
       return res.status(200).json(todo);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send('Server error!');
+    }
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  async updateTodo(req: Request, res: Response): Promise<any> {
+    try {
+      const { id } = req.params;
+      const { finished } = req.body;
+
+      await Todo.findByIdAndUpdate(id, {
+        finished,
+      });
+
+      const todo = await Todo.findById(id);
+      return res.status(200).json(todo);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send('Server error!');
+    }
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  async deleteTodo(req: Request, res: Response): Promise<any> {
+    try {
+      const { id } = req.params;
+
+      await Todo.findByIdAndDelete(id);
+
+      return res.status(204).send();
     } catch (error) {
       console.log(error);
       return res.status(500).send('Server error!');
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,8 @@ import TodoController from '../api/controllers/todoController';
 const router = Router();
 
 router.get('/todos', TodoController.getAllTodos);
-router.post('/todos', TodoController.createTodos);
+router.post('/todos', TodoController.createTodo);
+router.put('/todos/:id', TodoController.updateTodo);
+router.delete('/todos/:id', TodoController.deleteTodo);
 
 export default router;
